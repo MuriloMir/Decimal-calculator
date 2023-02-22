d648bf8613e48507e526ce8b5b6c6e74024c88b4
Murilo Miranda <murilomiranda92@hotmail.com>
2023-02-22 00:03:27 -0300

    Source code.

--- decimalcal.d
+++ decimalcal.d
@@ -1,238 +1,166 @@
-// This module has functions to do calculations using the decimal system, thus eliminating errors due to the imprecise binary system.
+// This module has functions to do calculations using the decimal system, thus eliminating errors caused by the imprecise binary system.
 module decimalcal;
 
-import std.algorithm.searching : canFind;
 import std.array : replace, split;
 import std.bigint : BigInt;
 import std.conv : to;
 import std.string : format;
 
-// this function adds the necessary padding zeros
+// this is how many decimal places it is able to show, it rounds the last one only if it exceeds this limit
+const int precision = 20;
+
+// this routine adds the necessary padding zeros so the operands will have the same number of decimal digits
 void zeroing(ref string a, ref string b)
 {
-    // choosing the longest one, we check if it is the integer or the decimal part
-    if (a.length < b.length)
-        while (a.length < b.length)
-            if (canFind(a, '.'))
-                a ~= '0';
-            else
-                a = '0' ~ a;
+    // extracting the decimal part of the strings
+    string aDecimal = split(a, '.')[1], bDecimal = split(b, '.')[1];
+    // we do it based on the longest one
+    if (aDecimal.length < bDecimal.length)
+        a ~= format("%0*d", bDecimal.length - aDecimal.length, 0);
+    else if (aDecimal.length > bDecimal.length)
+        b ~= format("%0*d", aDecimal.length - bDecimal.length, 0);
+}
+
+// this routine will be used to round the last digit, if necessary
+void roundLastDigit(ref string result)
+{
+    // we calculate how far beyond the precision we are
+    ulong difference = split(result, '.')[1].length - precision;
+    // we check to see if the digit which comes right after the precision is greater than or equal to 5
+    if (result[$ - difference] >= '5')
+        // then we round it up, we have to make sure it doesn't create an infinite recursion
+        result = addOrSubtract(result[0 .. $ - difference], format("0.%0*d", precision, 1), '+');
+    // or we round it down
     else
-        while (b.length < a.length)
-            if (canFind(b, '.'))
-                b ~= '0';
-            else
-                b = '0' ~ b;
-    // in case the numbers are negative, the loop above pushed the "-" sign away
-    if (canFind(a, '-'))
-        a = '-' ~ replace(a, "-", "");
-    if (canFind(b, '-'))
-        b = '-' ~ replace(b, "-", "");
+        result = result[0 .. $ - difference];
 }
 
-// this function organizes the numbers so they will have the same number of place values
-void organize(ref string a, ref string b)
+// this function will remove the extra 0s in the left and in the right of the result
+string removeExtraZeros(string resultArg)
 {
-    // this is to remove a possible "+" sign in front of the numbers
-    if (a[0] == '+')
-        a = a[1 .. $];
-    if (b[0] == '+')
-        b = b[1 .. $];
-    // in case there is no radix point
-    if (!canFind(a, '.'))
-        a ~= '.';
-    if (!canFind(b, '.'))
-        b ~= '.';
-    // in case there is no integer digit before the radix point
-    if (a[0] == '.')
-        a = '0' ~ a;
-    if (b[0] == '.')
-        b = '0' ~ b;
-    // in case you write a number like "-.08", we must add a 0 before the radix point
-    if (a[0] == '-' && a[1] == '.')
-        a = "-0" ~ a[1 .. $];
-    if (b[0] == '-' && b[1] == '.')
-        b = "-0" ~ b[1 .. $];
-    // breaking the strings into integer and decimal part
-    string[2] aPieces = split(a, '.'), bPieces = split(b, '.');
-    // selecting the integer and decimal part
-    string opaInt = aPieces[0], opaDec = '.' ~ aPieces[1];
-    string opbInt = bPieces[0], opbDec = '.' ~ bPieces[1];
-    // adding the necessary padding zeros to the numbers
-    zeroing(opaDec, opbDec);
-    zeroing(opaInt, opbInt);
-    // updating the operands with their organized form
-    a = opaInt ~ opaDec;
-    b = opbInt ~ opbDec;
+    // we keep removing doubled 0s in the left
+    while (resultArg[0 .. 2] == "00")
+        resultArg = resultArg[1 .. $];
+    // then we keep removing doubled 0s in the right
+    while (resultArg[$ - 2 .. $] == "00")
+        resultArg = resultArg[0 .. $ - 1];
+    return resultArg;
 }
 
-// this function pushes the radix point to the very end
-void bubbleRadixes(ref string a, ref string b)
+// this function adds or subtracts the operands
+string addOrSubtract(string a, string b, char operator)
 {
-    a = replace(a, ".", "") ~ '.';
-    b = replace(b, ".", "") ~ '.';
-}
-
-// this function adds the operands
-string add(string a, string b)
-{
-    // this is how many decimal places it is able to show, it rounds the last one only if it reaches this limit
-    int precision = 21;
+    // we change the operations in case you are adding negative operands
+    if (operator == '+')
+        // if b is negative then we do a - |b| because x + -y = x - y
+        if (b[0] == '-')
+            return addOrSubtract(a, b[1 .. $], '-');
+        // if b is positive but a is negative then we do b - |a| because -x + y = y - x
+        else if (a[0] == '-')
+            return addOrSubtract(b, a[1 .. $], '-');
+    // we change the operations in case you are subtracting and you have negative operands or you have b > a
+    if (operator == '-')
+        // if b is negative then in this case we do a + |b| because x - -y = x + y
+        if (b[0] == '-')
+            return addOrSubtract(a, b[1 .. $], '+');
+        // if b is positive but a is negative then we do - (|a| + b) because -x - y = - (x + y)
+        else if (a[0] == '-')
+            return '-' ~ addOrSubtract(a[1 .. $], b, '+');
+        // if you are trying to subtract a number from another one smaller then we swap the operation and add the negative sign in front of it
+        else if (BigInt(replace(a, ".", "")) < BigInt(replace(b, ".", "")))
+            return '-' ~ addOrSubtract(b, a, '-');
     // first we organize them so they will have the same number of decimal places
-    organize(a, b);
+    zeroing(a, b);
     // now we store the number of decimal places that will be removed from both of them to make them integers
-    ulong n = split(a, '.')[1].length;
-    // here we bubble the radix point so they will be integers
-    bubbleRadixes(a, b);
-    // now we store the number's size (without the radix point)
-    ulong size = a.length - 1;
-    // here we create the result string with the sum of the operands as integers
-    string result = to!string(BigInt(a[0 .. $ - 1]) + BigInt(b[0 .. $ - 1]));
-    // this is for cases when you add something like 0.0001 + 0.0002
-    while (result.length < size)
-        result = '0' ~ result;
-    // this is to check if the result was negative so we can work with the "-" sign
-    bool negative = canFind(result, '-');
-    // this is for the case when there was a "-" sign in the result, the loop above just pushed it away from the beginning
-    if (negative)
-        result = '-' ~ replace(result, "-", "");
+    ulong decimalPlaces = split(a, '.')[1].length;
+    // here we remove the radix point so they will be integers
+    a = replace(a, ".", ""), b = replace(b, ".", "");
+    // now we store the number's size
+    ulong size = a.length >= b.length ? a.length : b.length;
+    // here we create the result string with the sum or difference of the operands as integers
+    string result = to!string(operator == '+' ? BigInt(a) + BigInt(b) : BigInt(a) - BigInt(b));
+    // this is for cases when you add or subtract numbers like 0.0001 and 0.0002, we add the zeros in front of the result
+    if (result.length < size)
+        result = format("%0*d", size - result.length, 0) ~ result;
     // now we add the radix point in the correct place
-    n = result.length - n;
-    result = result[0 .. n] ~ '.' ~ result[n .. $];
-    // this is to round the last digit and remove it afterwards, we make sure it doesn't create an infinite recursion
-    if (split(result, '.')[1].length >= 21)
-    {
-        result = result[0 .. $ - (split(result, '.')[1].length - 21)];
-        if (result[$ - 1] >= '5')
-            result = add(result[0 .. $ - 1], "0.00000000000000000001");
-        else
-            result = result[0 .. $ - 1];
-    }
-    // this is to make sure it has at least 1 digit before and after the radix point
-    if (result[$ - 1] == '.')
-        result ~= '0';
-    else if (result[0] == '.')
-        result = '0' ~ result;
-    return result;
-}
-
-// this function subtracts the operands
-string subtract(string a, string b)
-{
-    // this is how many decimal places it is able to show, it rounds the last one only if it reaches this limit
-    int precision = 21;
-    // first we organize them so they will have the same number of decimal places
-    organize(a, b);
-    // now we store the number of decimal places that will be removed from both of them to make them integers
-    ulong n = split(a, '.')[1].length;
-    // here we bubble the radix point so they will be integers
-    bubbleRadixes(a, b);
-    // now we store the number's size (without the radix point)
-    ulong size = a.length - 1;
-    // here we create the result string with the difference of the operands as integers
-    string result = to!string(BigInt(a[0 .. $ - 1]) - BigInt(b[0 .. $ - 1]));
-    // this is for cases when you subtract something like 0.0002 - 0.0001
-    while (result.length < size)
-        result = '0' ~ result;
-    // this is to check if the result was negative so we can work with the "-" sign
-    bool negative = canFind(result, '-');
-    // this is for the case when there was a "-" sign in the result, the loop above just pushed it away from the beginning
-    if (negative)
-        result = '-' ~ replace(result, "-", "");
-    // now we add the radix point at the correct place, notice the length of the result here may be different from before
-    // in case you subtract 2 positive numbers and the result is negative, in this case the "-" sign will make it longer
-    n = result.length - n;
-    result = result[0 .. n] ~ '.' ~ result[n .. $];
-    // this is to round the last digit and remove it afterwards
-    if (split(result, '.')[1].length >= 21)
-    {
-        result = result[0 .. $ - (split(result, '.')[1].length - 21)];
-        if (result[$ - 1] >= '5')
-            result = add(result[0 .. $ - 1], "0.00000000000000000001");
-        else
-            result = result[0 .. $ - 1];
-    }
-    // this is in cases when you subtract negative numbers, the result turns out positive and you are left with an extra 0
-    if (result[0] == '0')
-        result = result[1 .. $];
-    // this is to make sure it has at least 1 digit before and after the radix point
-    if (result[$ - 1] == '.')
-        result ~= '0';
-    else if (result[0] == '.')
-        result = '0' ~ result;
-    return result;
+    decimalPlaces = size - decimalPlaces;
+    result = result[0 .. decimalPlaces] ~ '.' ~ result[decimalPlaces .. $];
+    // this is to round the last digit and remove it afterwards, we check if the decimal part went beyond the precision
+    if (split(result, '.')[1].length > precision)
+        roundLastDigit(result);
+    // we remove doubled extra 0s before returning it
+    return removeExtraZeros(result);
 }
 
 // this function multiplies the operands
 string multiply(string a, string b)
 {
-    // this is how many decimal places it is able to show, it rounds the last one only if it reaches this limit
-    int precision = 21;
-    // first we organize them so they will have the same number of decimal places
-    organize(a, b);
-    // now we store the number of decimal places that will be removed from both of them to make them integers
-    ulong n = split(a, '.')[1].length * 2;
-    // here we bubble the radix point so they will be integers
-    bubbleRadixes(a, b);
-    // now we multiply them as integers and store as a string
-    string result = format(format("%%0%dd", n + 1), BigInt(a[0 .. $ - 1]) * BigInt(b[0 .. $ - 1]));
-    // now we add the radix point at the correct place
-    n = result.length - n;
-    result = result[0 .. n] ~ '.' ~ result[n .. $];
-    // this is to make sure it doesn't have unnecessary zeros at the end
-    while (result[$ - 1] == '0')
-        result = result[0 .. $ - 1];
-    // this is to round the last digit and remove it afterwards
-    if (split(result, '.')[1].length >= 21)
-    {
-        result = result[0 .. $ - (split(result, '.')[1].length - 21)];
-        if (result[$ - 1] >= '5')
-            result = add(result[0 .. $ - 1], "0.00000000000000000001");
+    // we change the operations in case you are multiplying negative operands
+    if (a[0] == '-')
+        // if both are negatives then the result will be |a| * |b|
+        if (b[0] == '-')
+            return multiply(a[1 .. $], b[1 .. $]);
+        // if only a is negative then the result will be -(|a| * b)
         else
-            result = result[0 .. $ - 1];
-    }
-    // this is to make sure it has at least 1 digit before and after the radix point
-    if (result[$ - 1] == '.')
-        result ~= '0';
-    else if (result[0] == '.')
-        result = '0' ~ result;
-    return result;
+            return '-' ~ multiply(a[1 .. $], b);
+    // if only b is negative then the result will be -(a * |b|)
+    else if (b[0] == '-')
+        return '-' ~ multiply(a, b[1 .. $]);
+    // now we store the number of decimal places that will be removed from both of them to make them integers, we use both of them because are doing multiplication here
+    ulong decimalPlaces = split(a, '.')[1].length + split(b, '.')[1].length;
+    // here we remove the radix point so they will be integers
+    a = replace(a, ".", ""), b = replace(b, ".", "");
+    // here we create the result string with the product of the operands as integers
+    string result = to!string(BigInt(a) * BigInt(b));
+    // in case the result was shorter than the number of decimal places, you need to add an extra 0 here
+    if (result.length < decimalPlaces + 1)
+        result = format("%0*d", decimalPlaces + 1, 0) ~ result;
+    // now we add the radix point in the correct place
+    result = result[0 .. $ - decimalPlaces] ~ '.' ~ result[$ - decimalPlaces .. $];
+    // this is to round the last digit and remove it afterwards, we check if the decimal part went beyond the precision
+    if (split(result, '.')[1].length > precision)
+        roundLastDigit(result);
+    // we remove doubled extra 0s before returning it
+    return removeExtraZeros(result);
 }
 
 // this function divides the second operand into the first as in a long division
 string divide(string a, string b)
 {
-    // this is how many decimal places it is able to show, it rounds the last one only if it reaches this limit
-    int precision = 21;
+    // we change the operations in case you are dividing negative operands
+    if (a[0] == '-')
+        // if both are negatives then the result will be |a| / |b|
+        if (b[0] == '-')
+            return divide(a[1 .. $], b[1 .. $]);
+        // if only a is negative then the result will be -(|a| / b)
+        else
+            return '-' ~ divide(a[1 .. $], b);
+    // if only b is negative then the result will be -(a / |b|)
+    else if (b[0] == '-')
+        return '-' ~ divide(a, b[1 .. $]);
     // first we organize them so they will have the same number of decimal places
-    organize(a, b);
-    // here we bubble the radix point so they will be integers
-    bubbleRadixes(a, b);
+    zeroing(a, b);
+    // here we remove the radix point so they will be integers
+    a = replace(a, ".", ""), b = replace(b, ".", "");
     // here I create an integer version of the divisor because it makes the code cleaner than casting it many times
-    BigInt divisor = BigInt(b[0 .. $ - 1]);
+    BigInt divisor = BigInt(b);
+    // in case something goes wrong and you try to divide by 0
     if (divisor == 0)
         return "division by 0 is impossible";
     //the string that holds the quotient, we start with the integer part of the quotient
-    string result = to!string(BigInt(a[0 .. $ - 1]) / divisor) ~ '.';
-    // here we do the decimal part of the quotient according to the precision passed as argument
-    for (BigInt rest = BigInt(a[0 .. $ - 1]) % divisor; rest && precision--; rest %= divisor)
+    string result = to!string(BigInt(a) / divisor) ~ '.';
+    // here we do the decimal part of the quotient according to the precision, for as long as there is a remainder
+    BigInt remainder = BigInt(a) % divisor;
+    for (int i; remainder > 0 && i++ < precision; remainder %= divisor)
     {
-        rest *= 10;
-        result ~= to!string(rest / divisor);
+        remainder *= 10;
+        result ~= to!string(remainder / divisor);
     }
-    // this is to round the last digit and remove it afterwards
-    if (split(result, '.')[1].length >= 21)
-    {
-        result = result[0 .. $ - (split(result, '.')[1].length - 21)];
-        if (result[$ - 1] >= '5')
-            result = add(result[0 .. $ - 1], "0.00000000000000000001");
-        else
-            result = result[0 .. $ - 1];
-    }
-    // this is to make sure it has at least 1 digit before and after the radix point
-    if (result[$ - 1] == '.')
-        result ~= '0';
-    else if (result[0] == '.')
-        result = '0' ~ result;
+    // we have to do it one last time to make sure it will have at least 1 digit after the radix point
+    result ~= to!string(remainder * 10 / divisor);
+    // this is to round the last digit and remove it afterwards, we check if the decimal part went beyond the precision
+    if (split(result, '.')[1].length > precision)
+        roundLastDigit(result);
+    // there is no way it could have extra 0s here, so we just return it right away
     return result;
 }
